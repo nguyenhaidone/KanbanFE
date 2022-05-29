646154ac46f55c99ffc8d9662dfe7300103f0362
Nguyen Hai Dang <dang682000@gmail.com>
2022-05-29 20:48:20 +0700

    fix-no-console-eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,8 +10,8 @@ module.exports = {
       ecmaVersion: 2020
     },
     rules: {
-      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-      'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+      'no-console': 'off',
+      'no-debugger': 'off',
       'vue/no-deprecated-slot-attribute': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
       'no-unused-vars': 'off',
